a451cb5f50dfdd2bf429563cccacc7f66df7657c
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2025-03-10 20:39:58 +0100

    Remove namespaces

--- src/pangram/main.cpp
+++ src/pangram/main.cpp
@@ -8,10 +8,10 @@ using namespace std;
 int main(int argc, char** argv) {
 
   // Test strings
-  std::string case_0 = "A quick brown fox jumps over the lazy dog";
-  std::string case_1 = "When zombies arrive quickly, fax judge Pat";
-  std::string case_2 = "The five boxing wizards jump quickly";
-  std::string case_3 = "Amazingly few discotheques provide jukeboxes";
+  string case_0 = "A quick brown fox jumps over the lazy dog";
+  string case_1 = "When zombies arrive quickly, fax judge Pat";
+  string case_2 = "The five boxing wizards jump quickly";
+  string case_3 = "Amazingly few discotheques provide jukeboxes";
 
   for(string s : vector<string> {case_0, case_1, case_2, case_3}) {
     cout << "\"" + s + "\"" + " is " + (pangram(s) ? "" : "not ") + "a pangram" << endl;
